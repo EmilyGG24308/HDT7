6609c3a961ecbd47a660339afebcc955d563eafa
EmilyGG24308 <gon24308@uvg.edu.gt>
2025-03-27 22:11:47 -0600

    Update Producto.java

--- Producto.java
+++ Producto.java
@@ -1,3 +1,28 @@
 import.java.util.Map;
 
 //Producto en el inventario
+
+class Producto{
+  String name;
+  String sku;
+  String description;
+  Map<String, Integer> sizes;
+
+  public Producto(String sku, String name, String description, Map<String, Integer> sizes) {
+      this.sku = sku;
+      this.name = name;
+      this.description = description;
+      this.sizes = sizes;
+  }
+
+  public void updateDescription(String newDescription) {
+      this.description = newDescription;
+  }
+
+  public void updateSizes(Map<String, Integer> newSizes) {
+      this.sizes = newSizes;
+  }
+}
+
+  
+  
